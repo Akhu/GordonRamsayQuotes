09c8fccc42b0c75e951e0deb9257a802742e2c97
Anthony Da Cruz <anthony.dacruz@aboutgoods.net>
2017-11-17 08:17:15 +0100

    Responsive : OK
    Responsive + mobile copy/paste done ✅
    Todo :
    - goods URL everywhere
    - Copy/past from JS
    - Deploy

--- resources/views/quote.blade.php
+++ resources/views/quote.blade.php
@@ -5,6 +5,13 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/manifest.json">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="theme-color" content="#ffffff">
+
         <!-- OGs -->
         <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
         <meta property="og:type"               content="website" />
@@ -33,13 +40,15 @@
                     <div class="links">
                         <a class="btn twitter" href="https://twitter.com/intent/tweet?text={{ $quote->content . " - Gordon Ramsay" }}&via=gordonramsayquotes.com">Share on Twitter</a>
                         <a class="btn clipboard" onclick="copy('{{ $quote->content . " - Gordon Ramsay" }}')" href="">Copy</a>
+                        <div class="copy-mobile">
+                            <span>Select and copy to share</span>
+                            <input type="text" value="{{ url('/' . $quote->id) }}" class="input-copy"/>
+                        </div>
                     </div>
                 </div>
             </div>
-            
         </div>
         <footer>
-            
             <div>Made with love by <a href="">@anthokhun</a></div>
             <div>This is not affiliated with Gordon Ramsay, I just like his way of managing people</div>
             <div><a href="">Contact</a></div>
